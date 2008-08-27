6fec84e7dfc96c62ae327dd8da3c83b549dd94aa
Harlan Iverson <h.iverson@gmail.com>
2008-08-27 02:55:07 -0500

    removed target

--- soashable-web/target/soashable-web-2.0-SNAPSHOT/scripts/model/RosterModel.js
+++
@@ -1,9 +0,0 @@
-dojo.provide("soashable.model.RosterModel");
-
-/**
- * The model that represents a users' roster
- */
-dojo.declare( "soashable.model.RosterModel", Xmpp4Js.Event.EventProvider, {
-    constructor: function() {
-    }
-});
--- soashable-web/target/soashable-web-2.0-SNAPSHOT/scripts/widget/__package__.js
+++
@@ -1,9 +0,0 @@
-dojo.kwCompoundRequire({
-    common: [
-        "soashable.widget.StatusBox",
-        "soashable.widget.DemoWindow"
-    ],
-    browser: [    ]
-});
-
-dojo.provide("soashable.widget.*");
